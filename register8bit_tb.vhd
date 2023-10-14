c6ddc132a8325df853a34be8cdc19a593a361c1c
Syed Yead Zaman <s.yead.zaman@gmail.com>
2023-10-13 23:47:16 -0400

    updated register8bit tb

--- register8bit_tb.vhd
+++ register8bit_tb.vhd
@@ -60,11 +60,18 @@ begin
     assert data_out_tb = "00000000" report "Data mismatch at reset" severity error;
 
     reset_tb <= '0';
+    wait for period;
 
-    enable_tb  <= '0';
+    enable_tb  <= '1';
     data_in_tb <= "00000010";
     wait for period;
-    assert data_out_tb = "00000000" report "Data mismatch at enable off" severity error;
+    assert data_out_tb = "00000010" report "Data mismatch at enable off" severity error;
+
+    enable_tb  <= '0';
+    data_in_tb <= "00000001";
+    wait for period;
+    assert data_out_tb = "00000010" report "Data mismatch at enable off" severity error;
+
 
     data_in_tb <= "00000000";
     wait for period;
@@ -72,8 +79,8 @@ begin
     enable_tb <= '1';
     for i in 0 to 255 loop
       data_in_tb <= std_logic_vector(to_unsigned(i, 8));
-      assert data_out_tb = data_in_tb report "Data mismatch at address " & integer'image(i) severity error;
       wait for period;
+      assert data_out_tb = data_in_tb report "Data mismatch at address " & integer'image(i) severity error;
     end loop;
 
     sim_end <= true;
