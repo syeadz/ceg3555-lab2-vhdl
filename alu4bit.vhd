10e13f353332ca9827cd93cd28d6ea1ef20f92c0
Syed Yead Zaman <s.yead.zaman@gmail.com>
2023-10-20 19:01:02 -0400

    added dec hex

--- alu4bit.vhd
+++ alu4bit.vhd
@@ -8,7 +8,8 @@ entity alu4bit is
     b       : buffer std_logic_vector(3 downto 0);
     op      : in std_logic_vector(1 downto 0);
     f       : out std_logic_vector(7 downto 0);
-    c, z, v : out std_logic
+    c, z, v : out std_logic;
+    hexa1, hexa2, hexb1, hexb2, hexf1, hexf2, hexf3, hexf4 : out std_logic_vector(6 downto 0)
   );
 end;
 
@@ -19,6 +20,16 @@ begin
   sub_op <= not op(0) and op(1);
   mul_op <= op(0) and not op(1);
   div_op <= op(0) and op(1);
+
+  dec_hexa1 : entity work.dec_7seg port map (
+        i_hexDigit => a,
+        o_segment_a => hexa1(0),
+        o_segment_b => hexa1(1),
+        o_segment_c => hexa1(2),
+        o_segment_d => hexa1(3),
+        o_segment_e => hexa1(4),
+        o_segment_f => hexa1(5)
+    );
 
   adder : entity work.adder4bit port map
     (
