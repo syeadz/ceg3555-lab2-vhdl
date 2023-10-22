b015f63c7e95555a5e341bfca319b61d1bde81d8
Syed Yead Zaman <s.yead.zaman@gmail.com>
2023-10-21 22:58:57 -0400

    made shift register use mux

--- shftleftreg4bit.vhd
+++ shftleftreg4bit.vhd
@@ -6,7 +6,7 @@ entity shftleftreg4bit is
   (
     clk      : in std_logic;
     reset    : in std_logic;
-    enable   : in std_logic;
+    load     : in std_logic;
     data_in  : in std_logic_vector(3 downto 0);
     data_out : out std_logic_vector(3 downto 0);
     shift    : in std_logic
@@ -15,21 +15,90 @@ end;
 
 architecture rtl of shftleftreg4bit is
   signal int_data_in : std_logic_vector(3 downto 0);
-  signal int_data_out: std_logic_vector(3 downto 0);
-begin
-  int_data_in(0) <= (data_in(0) and not shift) or (data_in(3) and shift);
-  int_data_in(1) <= (data_in(1) and not shift) or (data_in(0) and shift);
-  int_data_in(2) <= (data_in(2) and not shift) or (data_in(1) and shift);
-  int_data_in(3) <= (data_in(3) and not shift) or (data_in(2) and shift);
+  signal not_reset   : std_logic;
 
-  reg4bit : entity work.register4bit port map
+  component enARdFF_2
+    port
     (
-      clk => clk,
-      reset => reset,
-      enable => enable,
-      data_in => int_data_in,
-      data_out => int_data_out
-    );
+      i_resetBar  : in std_logic;
+      i_d         : in std_logic;
+      i_enable    : in std_logic;
+      i_clock     : in std_logic;
+      o_q, o_qBar : out std_logic);
+  end component;
+begin
+  not_reset <= not reset;
 
-    data_out <= int_data_out;
-end rtl;
+  mux3 : entity work.mux2to1 port map
+    (
+    sel    => shift,
+    d0     => data_in(3),
+    d1     => data_in(2),
+    enable => '1',
+    q      => int_data_in(3));
+
+  ff3 : enARdFF_2
+  port
+  map
+  (
+  i_resetBar => not_reset,
+  i_d        => int_data_in(3),
+  i_enable   => load,
+  i_clock    => clk,
+  o_q        => data_out(3),
+  o_qBar     => open);
+
+  mux2 : entity work.mux2to1 port
+    map
+    (
+    sel    => shift,
+    d0     => data_in(2),
+    d1     => data_in(1),
+    enable => '1',
+    q      => int_data_in(2));
+
+  ff2 : enARdFF_2
+  port
+  map (i_resetBar => not_reset,
+  i_d             => int_data_in(2),
+  i_enable        => load,
+  i_clock         => clk,
+  o_q             => data_out(2),
+  o_qBar          => open);
+
+  mux1 : entity work.mux2to1 port
+    map
+    (
+    sel    => shift,
+    d0     => data_in(1),
+    d1     => data_in(0),
+    enable => '1',
+    q      => int_data_in(1));
+
+  ff1 : enARdFF_2
+  port
+  map (i_resetBar => not_reset,
+  i_d             => int_data_in(1),
+  i_enable        => load,
+  i_clock         => clk,
+  o_q             => data_out(1),
+  o_qBar          => open);
+
+  mux0 : entity work.mux2to1 port map (
+    sel    => shift,
+    d0     => data_in(0),
+    d1     => data_in(3),
+    enable => '1',
+    q      => int_data_in(0)
+  );
+
+  ff0 : enARdFF_2
+  port
+  map (i_resetBar => not_reset,
+  i_d             => int_data_in(0),
+  i_enable        => load,
+  i_clock         => clk,
+  o_q             => data_out(0),
+  o_qBar          => open);
+
+end;
--- shftleftreg4bit_tb.vhd
+++ shftleftreg4bit_tb.vhd
@@ -13,8 +13,6 @@ architecture rtl of shftleftreg4bit_tb is
   signal data_out_tb : std_logic_vector(3 downto 0);
   signal shift_tb    : std_logic;
 
-  signal data_should_be : std_logic_vector(3 downto 0);
-
   signal sim_end  : boolean := false;
   constant period : time    := 50 ns;
 begin
@@ -23,7 +21,7 @@ begin
     (
       clk      => clk_tb,
       reset    => reset_tb,
-      enable   => enable_tb,
+      load     => enable_tb,
       data_in  => data_in_tb,
       data_out => data_out_tb,
       shift    => shift_tb
@@ -42,45 +40,44 @@ begin
 
   stim_proc : process
   begin
-    shift_tb  <= '0';
-    enable_tb <= '0';
-    reset_tb  <= '1', '0' after period;
-    wait for period;
-    assert data_out_tb = "0000" report "Data mismatch at reset" severity error;
+    -- shift_tb  <= '0';
+    -- enable_tb <= '0';
+    -- reset_tb  <= '1', '0' after period;
+    -- wait for period;
+    -- assert data_out_tb = "0000" report "Data mismatch at reset" severity error;
 
-    data_in_tb <= "0001";
-    wait for period;
-    assert data_out_tb = "0000" report "Data mismatch at reset" severity error;
+    -- data_in_tb <= "0001";
+    -- wait for period;
+    -- assert data_out_tb = "0000" report "Data mismatch at reset" severity error;
 
-    wait for period/2;
-    enable_tb <= '1';
-    wait for period/2;
-    assert data_out_tb = "0000" report "Data mismatch at clock off" severity error;
+    -- wait for period/2;
+    -- enable_tb <= '1';
+    -- wait for period/2;
+    -- assert data_out_tb = "0000" report "Data mismatch at clock off" severity error;
 
-    wait for period;
-    assert data_out_tb = "0010" report "Data mismatch at clock on, enable on" severity error;
+    -- wait for period;
+    -- assert data_out_tb = "0010" report "Data mismatch at clock on, enable on" severity error;
 
-    reset_tb <= '1';
-    wait for period;
-    assert data_out_tb = "0000" report "Data mismatch at reset" severity error;
+    -- reset_tb <= '1';
+    -- wait for period;
+    -- assert data_out_tb = "0000" report "Data mismatch at reset" severity error;
 
-    reset_tb <= '0';
-    wait for period;
+    -- reset_tb <= '0';
+    -- wait for period;
 
-    enable_tb  <= '0';
-    data_in_tb <= "0010";
-    wait for period;
-    assert data_out_tb = "0000" report "Data mismatch at enable off" severity error;
+    -- enable_tb  <= '0';
+    -- data_in_tb <= "0010";
+    -- wait for period;
+    -- assert data_out_tb = "0000" report "Data mismatch at enable off" severity error;
 
-    data_in_tb <= "0000";
-    wait for period;
+    -- data_in_tb <= "0000";
+    -- wait for period;
 
     -- shifting test
     enable_tb <= '1';
     shift_tb  <= '1';
     for i in 0 to 7 loop
       data_in_tb <= std_logic_vector(to_unsigned(i, 4));
-      wait for period;
       wait for period;
     end loop;
 
